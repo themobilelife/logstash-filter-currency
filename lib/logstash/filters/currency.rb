11eef3c387676db378ebba9f8a58ba9549f149c0
Gabriel <gabriel@themobilelife.com>
2016-11-10 12:03:47 +0100

    integrated CurrencyService API

--- lib/logstash/filters/currency.rb
+++ lib/logstash/filters/currency.rb
@@ -3,58 +3,67 @@ require "logstash/filters/base"
 require "logstash/namespace"
 require "json"
 require "open-uri"
+require "date"
 
 class LogStash::Filters::Currency < LogStash::Filters::Base
 
-  # Setting the config_name here is required. This is how you
-  # configure this filter from your Logstash config.
-
   config_name "currency"
 
-  # Replace the message with this value.
-  config :currency, :validate => :string, :default => ""
-  config :fields, :validate => :array, :default => ""
+  config :currency, :validate => :string
+  config :fields, :validate => :array
 
   public
   def register
-    # Add instance variables
-    @currencies = JSON.load(open("http://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json"))
-  end # def register
+    @fx = {}
+
+    if ENV["CURRENCY_SERVICE_HOST"].nil?
+      raise "Required environment variable CURRENCY_SERVICE_HOST was not set!"
+    end
+
+    @api_addr = ENV["CURRENCY_SERVICE_HOST"]
+  end
 
   public
   def filter(event)
 
-    if @currency and event["currency"] and @fields
-      
-      # pprice: "1.401070"
-      @fields.each_with_index do |element,index|
-        # Concat USD with airline currency
+    @fields.each do |field|
+      d = Date.parse(event['date'])
+      # Format date to DD/MM/YYYY
+      date = d.strftime("%Y-%m-%d")
 
-        rate = getRate("USD/" + event["currency"])
-
-        # Get airline USD currency rate?
-        rate2 = getRate("USD/" + @currency)
-        event["converted" + @fields[index].capitalize] = (event[@fields[index]].to_f / (rate.to_f/rate2.to_f))           
+      unless field in event
+        raise "The received event does not contain required key, #{field}!"
       end
 
-      event["convertedCurrency"] = @currency
+      # Because USD is always the base currency we must first get the base currency rate against USD
+      # For example if we wanted to do conversion SEK/EUR we would first get USD/SEK rate.
+      base_to_usd_rate = getRate(event['currency'], date)
+
+      # Then follow that up with the quote currency rate against USD
+      quote_to_usd_rate = getRate(@currency, date)
+
+      # Finally convert the amounts by first dividing the amount with the USD/BASE rate and multiplying that by the USD/QUOTE rate.
+      amount = event[field] / base_to_usd_rate * quote_to_usd_rate
+      event["converted" + field.capitalize] = amount
     end
 
     filter_matched(event)
-
   end
 
   public
-  def getRate(currency)
-      arr = @currencies["list"]["resources"]
-
-      arr.each_with_index do |element,index|
-          if (arr[index]["resource"]["fields"]["name"] == currency)
-            price = arr[index]["resource"]["fields"]["price"]
-            return price
-          end
+  def getRate(currency, date)
+    # Check if we have the rates for this date memoized first
+    if @fx.key?(date) && @fx[date]["rates"].key?(currency)
+      return @fx[date]["rates"][currency]
+    else
+      # Memoize the results
+      url = "http://#{@api_addr}/rates/#{date}"
+      @fx[date] = JSON.load(open(url))
+      if @fx[date]["rates"].key?(currency)
+        return @fx[date]["rates"][currency]
       end
-
-      return 1
+    end
+    # If we reach this point then we were unable to find the requested currency
+    raise "Rates for #{currency} could not be found!"
   end
-end # class LogStash::Filters::Example
+end
